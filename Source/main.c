9a6d0bbdef48bd7bbb05338620a417b5acc70c24
ATmega8 <liuyang.2014@foxmail.com>
2016-09-20 15:08:05 +0000

    USB数据发送函数测试

--- Source/main.c
+++ Source/main.c
@@ -9,24 +9,30 @@
 #include "queue.h"
 
 const uint8_t str[12] = "He said Hi!\n";
+const uint8_t str1[12] = "12 3456 789\n";
 
 void IDLE_Task(void* parameters)
 {
-	uint32_t i;
-	uint32_t len = 12;
-
 	while(1)
 	{
-		vTaskDelay(100);
+		vTaskDelay(1000);
 
 		if(usbDevice.state == CONFIGURED)
 		{
-			for(i = 0; i < 12; i++)
-			{
-				usbTxBuffer[i] = str[i];
-			}
+			USB_SendData(&str[0], 12);
+		}
+	}
+}
 
-			xQueueSend(usbTxBufferCountQueue, &len, 0);
+void IDLE1_Task(void* parameters)
+{
+	while(1)
+	{
+		vTaskDelay(1000);
+
+		if(usbDevice.state == CONFIGURED)
+		{
+			USB_SendData(&str1[0], 12);
 		}
 	}
 }
@@ -43,7 +49,8 @@ int main(void)
 	//usbTxCbuf = CircularBuffer_Create(128, sizeof(uint32_t));
 
 	xTaskCreate(USB_Task, "USB Task", 1024, NULL, 3, NULL);
-	xTaskCreate(IDLE_Task, "Idle Task", 1024, NULL, 1, NULL);
+	xTaskCreate(IDLE_Task, "Idle Task", 130, NULL, 1, NULL);
+	xTaskCreate(IDLE1_Task, "Idle Task", 130, NULL, 1, NULL);
 	vTaskStartScheduler();
 
 	while(1)
